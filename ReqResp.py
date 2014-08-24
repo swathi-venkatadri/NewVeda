48473127868d32ea1723ab28b5cc75e547ad1d1d
vijaygenius123 <vijaygenius123@gmail.com>
2014-08-24 18:01:55 +0530

    Initial Commit

---
+++ ReqResp.py
@@ -0,0 +1,22 @@
+import urllib2
+import urllib
+
+# Specify the url
+url = 'http://newveda.freeiz.com/Test.php?Name=Swathi'
+
+# This packages the request (it doesn't make it) 
+request = urllib2.Request(url)
+
+# Sends the request and catches the response
+response = urllib2.urlopen(request)
+
+# Extracts the response
+html = response.read()
+
+if(html.find("Welcome")>=0):
+       print "Access Granted"
+else:
+       print "Access Denied"
+
+# Print it out
+#print html 
