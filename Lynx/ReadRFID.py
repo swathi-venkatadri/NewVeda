4c94afdc027b7ad34670971cf0ae1a7dda1fe430
swathi-venkatadri <swathi.venkatadri@gmail.com>
2014-08-28 09:43:45 +0530

    Update ReadRFID.py

--- Lynx/ReadRFID.py
+++ Lynx/ReadRFID.py
@@ -1,8 +1,11 @@
 import serial
-
 ### Import Serial Module
-
-
-
-ser=serial.Serial('COM1',9600)
+ser=serial.Serial('COM13',9600)
 ## COM1 With Baud 9600
+data=ser.read(12)
+if data=="26008B2938BC":
+  print "Access Granted"
+elif data=="4B009929F00B":
+  print "Access Denied"
+else:
+  print "Car Stolen"
